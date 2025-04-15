18fad5336943ae9ca741b77556768a61856c6438
Oğuzhan <43236992+oguzhantanitmis@users.noreply.github.com>
2025-04-15 17:41:31 +0300

    Update mc-sw-start.sh

--- mc-sw-start.sh
+++ mc-sw-start.sh
@@ -1,55 +1,21 @@
 #!/bin/bash
-clear
-echo ""
-echo "1) proxy start"
-echo "2) Çakma lobi start"
-echo "3) Lobi start"
-echo "4) Skyblock start"
-echo "5) Tekblok start"
-echo "---------------------------"
-echo "Bir seçenek seçiniz örnk (1): "
-read numara
-case $numara in
-1)
-screen -dmS "proxy" bash -c "trap 'echo gotsigint' INT; cd /home/mc-servers/proxy ; ./start.sh;  bash"
-	echo ""
-	echo ""
-	echo -e "Proxy başlatıldı."
-	echo ""
-	echo ""
-;;
-2)
-screen -dmS "cakmalobi" bash -c "trap 'echo gotsigint' INT; cd /home/mc-servers/cakmalobi ; ./start.sh;  bash"
-	echo ""
-	echo ""
-	echo -e "Çakmalobi başlatıldı."
-	echo ""
-	echo ""
-;;
-3)
-screen -dmS "lobi" bash -c "trap 'echo gotsigint' INT; cd /home/mc-servers/lobi ; ./start.sh;  bash"
-	echo ""
-	echo ""
-	echo -e "Lobi başlatıldı."
-	echo ""
-	echo ""
-;;
-4)
-screen -dmS "skyblock" bash -c "trap 'echo gotsigint' INT; cd /home/mc-servers/skyblock ; ./start.sh;  bash"
-	echo ""
-	echo ""
-	echo -e "Skyblock başlatıldı."
-	echo ""
-	echo ""
-;;
-5)
-screen -dmS "tekblok" bash -c "trap 'echo gotsigint' INT; cd /home/mc-servers/tekblok ; ./start.sh;  bash"
-	echo ""
-	echo ""
-	echo -e "Tekblok başlatıldı."
-	echo ""
-	echo ""
-;;
-*)
-echo "--> Hatalı bir seçenek seçtiniz."
-esac
+cd /opt/mc-server/proxy
+
+JAVA="java"
+JAR=$(ls spigot*.jar | head -n 1)
+RAM="1G"
+FLAGS="-Duser.language=tr -Dfile.encoding=UTF-8 -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15"
+
+# Eğer "proxy" screen oturumu yoksa sunucuyu başlat.
+if ! screen -list | grep -q "\.proxy"; then
+    echo "Sunucu başlatılıyor..."
+    screen -dmS proxy ${JAVA} -Xmx${RAM} -Xms512M ${FLAGS} -jar ${JAR}
+fi
+
+# Screen oturumu çalışırken bekle.
+while screen -list | grep -q "\.proxy"; do
+    sleep 5
+done
+
+echo "Proxy screen oturumu kapandı."
+exit 1
